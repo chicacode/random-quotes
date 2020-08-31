9c01cc4b5078e1cc5aeb1b5bc6440f127c968f42
chicacode <gerahernan.al@gmail.com>
2020-08-31 10:42:02 +0200

    added add eventListener method

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log("En el Random Quotes");
+
+let button;
+button = document.getElementById("generate-button");
+
+function getQuote() {
+    console.log("Click me");
+}
+button.addEventListener("click", getQuote, false);
