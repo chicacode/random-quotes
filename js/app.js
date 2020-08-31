9d84993d1828ad74b37495b0e3ae695dfccdc81e
chicacode <gerahernan.al@gmail.com>
2020-08-31 13:20:30 +0200

    added functionality fetch api quotes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 console.log("En el Random Quotes");
 
-let button;
+let button, quote, author;
+const url = "https://type.fit/api/quotes";
 button = document.getElementById("generate-button");
+quote = document.getElementById("quote-display");
+author = document.getElementById("author");
 
-function getQuote() {
-    console.log("Click me");
-}
-button.addEventListener("click", getQuote, false);
+(function () {
+    fetch(url)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data);
+            button.addEventListener("click", function () {
+                var quotes = data;
+                let random = Math.floor(Math.random() * quotes.length);
+                console.log(random);
+
+                quote.textContent = quotes[random].text;
+                author.textContent = quotes[random].author;
+            })
+        })
+        .catch(error => console.log("Unable to get items" + error));
+})();
