9f0b70a0b271811dd542a862d04ffc426eb5a3ad
chicacode <gerahernan.al@gmail.com>
2020-08-31 13:50:06 +0200

    added iife functionality

--- js/app.js
+++ js/app.js
@@ -6,11 +6,11 @@ button = document.getElementById("generate-button");
 quote = document.getElementById("quote-display");
 author = document.getElementById("author");
 
+//IIFE
 (function () {
     fetch(url)
         .then(response => response.json())
         .then(function (data) {
-            console.log(data);
             button.addEventListener("click", function () {
                 var quotes = data;
                 let random = Math.floor(Math.random() * quotes.length);
